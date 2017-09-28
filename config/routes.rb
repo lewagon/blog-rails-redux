a6b3bafe6cf37a8876b916c18dc62d4c06bad823
Edward Schults <edward.schults@gmail.com>
2017-09-28 22:47:50 +0200

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  # application routing is converging to the homepage
+  # home.html.erb is the view where we inject our React App
   root to: 'pages#home'
   get "posts/:id", to: 'pages#home'
   get "posts/new", to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # API routing
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :posts, only: [ :index, :show, :create ]
+    end
+  end
 end
